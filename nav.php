7ef7b73baab227f4bbf7e96ce89fa4ba65a62fe7
Philipp Batroff <batroff@systopia.de>
2018-07-09 15:16:19 +0200

    added Systopia Header

--- nav.php
+++ nav.php
@@ -1,5 +1,21 @@
 <?php
 
+/*-------------------------------------------------------+
+| Navision API Tools                                     |
+| Heinrich Böll Stiftung                                 |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: P. Batroff (batroff@systopia.de)               |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
 require_once 'nav.civix.php';
 use CRM_Nav_ExtensionUtil as E;
 
