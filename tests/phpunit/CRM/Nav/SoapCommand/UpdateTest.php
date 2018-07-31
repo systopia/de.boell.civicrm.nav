fe651486b79780ad1c314bca846a3aec7559f172
Philipp Batroff <batroff@systopia.de>
2018-07-31 15:17:31 +0200

    Make update test dynamic (more or less)

--- tests/phpunit/CRM/Nav/SoapCommand/UpdateTest.php
+++ tests/phpunit/CRM/Nav/SoapCommand/UpdateTest.php
@@ -46,53 +46,22 @@ class CRM_Nav_SoapCommand_UpdateTest extends \PHPUnit_Framework_TestCase {
    * Gets the first record in the Navision Database and outputs the contents
    */
   public function testSoapUpdateCommand() {
-    $params = array('civiContact' =>
-      array(
-        'Key'                           => "12;xtkAAACHAQ==10;13199485500;",
-        'No'                            => "P103288",
-        'Address'                       => "12;xtkAAACHAQ==10;13199485500;",
-        'Address_2'                     => "",
-        'City'                          => "Berlin",
-        'Phone_No'                      => "",
-        'Country_Region_Code'           => "DE",
-        'Fax_No'                        => "",
-        'Post_Code'                     => "10559",
-        'E_Mail'                        => "",
-        'Home_Page'                     => "",
-        'Type'                          => "Person",
-        'Company_No'                    => "P103288",
-        'Company_Name'                  => "",
-        'First_Name'                    => "Valery",
-        'Middle_Name'                   => "Kälberer",
-        'Surname'                       => "",
-        'Job_Title'                     => "",
-        'Mobile_Phone_No'               => "",
-        'Salutation_Code'               => "HERR",
-        'E_Mail_2'                      => "",
-        'Delete_Flag'                   => "",
-        'Company_Name_2'                => "",
-        'Funktion'                      => "",
-        'Geburtsdatum'                  => "0001-01-01",
-        'Postfach'                      => "",
-        'PLZ_Postfach'                  => "",
-        'Ort_Postfach'                  => "",
-        'Private_Telefonnr'             => "030397476151",
-        'Private_Faxnr'                 => "030397476159",
-        'Private_E_Mail'                => "vkaelberer@applabs.de",
-        'Visible'                       => "Alle",
-        'Company_Adress'                => "Alt-Moabit 92",
-        'Company_Adress_2'              => "",
-        'Company_Post_Code'             => "10559",
-        'Company_City'                  => "Berlin",
-        'Company_Country_Region_Code'   => "DE",
-        'Entry_No'                      => "1",
-        'Version'                       => "BEFORE",
-        'Change_Type'                   => "Change",
-        '_TIMESTAMP'                    => "2018-05-14T14:25:34.463Z",
-        'Transferred'                   => '1',
-      ),
+    // get contact #1
+    $r_params = array(
+      "Entry_No" => 1,
     );
-    $testUpdateCommand = new CRM_Nav_SoapCommand_Update($params);
+    $readCommand = new CRM_Nav_SoapCommand_Read($r_params);
+    try{
+      $this->soapConnector->executeCommand($readCommand);
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+      throw new Exception("Read Command failed. Message: " . $e->getMessage());
+    }
+
+    $update_params = json_decode(json_encode($readCommand->getSoapResult()), TRUE);
+    $update_params['civiContact']['Transferred'] = 1;
+
+    $testUpdateCommand = new CRM_Nav_SoapCommand_Update($update_params);
     try{
       $this->soapConnector->executeCommand($testUpdateCommand);
     } catch (Exception $e) {
@@ -100,8 +69,7 @@ class CRM_Nav_SoapCommand_UpdateTest extends \PHPUnit_Framework_TestCase {
       throw new Exception("Update Command failed. Message: " . $e->getMessage());
     }
 
-    print "API call successful! Result: \n";
-    print_r($testUpdateCommand->getSoapResult());
+    print "Update API call successful! Result: \n";
+//    print_r($testUpdateCommand->getSoapResult());
   }
-
 }
