063ee11031bc30d69396d0cf9a2202d5c6c3b77b
Philipp Batroff <batroff@systopia.de>
2018-07-31 10:16:50 +0200

    added unittests for Read, ReadMultiple

---
+++ CRM/Nav/SoapCommand/ReadMultiple.php
@@ -0,0 +1,47 @@
+<?php
+/*-------------------------------------------------------+
+| Navision API Tools                                     |
+| Heinrich Böll Stiftung                                 |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: P. Batroff (batroff@systopia.de)               |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+/**
+ * Class CRM_Nav_SoapCommand_Read
+ * Implementation for a Navision SOAP Read Command
+ */
+class CRM_Nav_SoapCommand_ReadMultiple extends CRM_Nav_SoapCommand_BaseSoapCommand {
+
+  private $params;
+  /**
+   * CRM_Nav_SoapCommand_Read constructor.
+   *
+   * @param $searchParams
+   */
+  // TODO: Figure out the correct params here
+  public function __construct($readParams) {
+    // TODO: for now we just save params
+    // in the future this needs to be after a certain timestamp,
+    // and probably from an ID
+    $this->params = $readParams;
+  }
+
+  /**
+   * @param $soapClient
+   *
+   * @throws SoapFault
+   */
+  public function execute(&$soapClient) {
+    // TODO: Implement execute() method.
+    $this->result = $soapClient->ReadMultiple($this->params);
+  }
+}
---
+++ tests/phpunit/CRM/Nav/SoapCommand/ReadMultipleTest.php
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+use CRM_Nav_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * Test A Navision API Read based on the SOAP Connector class as well as the
+ * Read Soap Command
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class CRM_Nav_SoapCommand_ReadMultipleTest extends \PHPUnit_Framework_TestCase {
+
+  private $soapConnector;
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  public function setUp() {
+    $this->soapConnector = new CRM_Nav_SOAPConnector();
+    parent::setUp();
+  }
+
+    public function tearDown() {
+    parent::tearDown();
+  }
+
+  /**
+   * test the Navision API Read Command
+   * Gets the first record in the Navision Database and outputs the contents
+   * @throws Exception
+   */
+  public function testSoapReadMultipleCommand() {
+//    $threshold_date = strtotime("18.05.2018");
+
+    $threshold_date = new DateTime('2018-07-25');
+    $params = array( 'filter' =>
+        array(
+        "Field"     => "_TIMESTAMP",
+        "Criteria"  => "{$threshold_date->format('c')}",
+      ),
+        'setSize' => '10',
+    );
+    // $this->soapConnector = new CRM_Nav_SOAPConnector();
+    $testReadCommand = new CRM_Nav_SoapCommand_ReadMultiple($params);
+    try{
+      $this->soapConnector->executeCommand($testReadCommand);
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+      throw new Exception("Read Command failed. Message: " . $e->getMessage());
+    }
+
+    print "API call successful! Result: \n";
+    print_r($testReadCommand->getSoapResult());
+  }
+
+}
