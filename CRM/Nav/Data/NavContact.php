e4da7f3741a5cd4a6097544079c6766714dce9c1
Philipp Batroff <batroff@systopia.de>
2018-08-01 18:14:14 +0200

    more Verification and compare fucntion between navContact Objects

--- CRM/Nav/Data/NavContact.php
+++ CRM/Nav/Data/NavContact.php
@@ -43,6 +43,27 @@ class CRM_Nav_Data_NavContact extends CRM_Nav_Data_NavDataRepresentationBase {
         return FALSE;
       }
     }
+    // Verify that always a Version is available
+    if (!$this->verify_version($navision_data)) {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+  /**
+   * check if a version is available and the Version is either
+   * BEFORE or AFTER
+   * @param $data
+   *
+   * @return bool
+   */
+  private function verify_version($data) {
+    if (!isset($data['Version'])) {
+      return FALSE;
+    }
+    if ($data['Version'] !== 'BEFORE' and  $data['Version'] !== 'AFTER')) {
+      return FALSE;
+    }
     return TRUE;
   }
 
@@ -58,5 +79,16 @@ class CRM_Nav_Data_NavContact extends CRM_Nav_Data_NavDataRepresentationBase {
     // TODO: Compare Objects
     // return array with data (key->val) from $other_nav_data that is different
     // than in  this object
+    $other_data = $other_nav_data->getNavData();
+    if ($this->nav_data['Version'] === 'BEFORE' and $other_data['Version'] === 'AFTER') {
+      $result = array_diff($this->nav_data, $other_data);
+    } else if ($this->nav_data['Version'] === 'AFTER' and $other_data['Version'] === 'BEFORE') {
+      $result = array_diff($other_data, $this->nav_data);
+    } else {
+      throw new Exception("Not fitting Version of Navision Data. unable to compare");
+    }
+    // we don't need Version - it should ALWAYS be different!
+    unset($result['Version']);
+    return $result;
   }
 }
