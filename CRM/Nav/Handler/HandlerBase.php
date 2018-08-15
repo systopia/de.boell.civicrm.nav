279d82ab558ba2ffcfa8d3fecce87bf900ca67dc
Philipp Batroff <batroff@systopia.de>
2018-08-15 17:58:25 +0200

    mock code

---
+++ CRM/Nav/Handler/HandlerBase.php
@@ -0,0 +1,24 @@
+<?php
+/*-------------------------------------------------------+
+| Navision API Tools                                     |
+| Heinrich Böll Stiftung                                 |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: P. Batroff (batroff@systopia.de)               |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+abstract class CRM_NAV_Handler_HandlerBase {
+
+  public function __construct() {
+  }
+
+  abstract public function process($record);
+}
