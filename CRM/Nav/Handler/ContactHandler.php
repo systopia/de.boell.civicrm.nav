308362d7b7d0adab236f61e006a37297e557e00c
Philipp Batroff <batroff@systopia.de>
2018-08-29 00:50:12 +0200

    WiP - Contact comparison handler

--- CRM/Nav/Handler/ContactHandler.php
+++ CRM/Nav/Handler/ContactHandler.php
@@ -41,21 +41,76 @@ class CRM_Nav_Handler_ContactHandler extends CRM_NAV_Handler_HandlerBase {
 
     $changed_entities = $this->get_update_values();
 
+    $this->check_nav_before_vs_civi($changed_entities, $contact_id);
+//    check all *_old values against current civi data
+
     // FIXME: THIS ISNT SUPPOSED TO HAPPEN RIGHT NOW!
-//    $this->add_entities_to_civicrm($changed_entities);
+    //    $this->add_entities_to_civicrm($changed_entities);
 
     // Check ob die alten Werte so schon in CiviCRM vorhanden sind
   }
 
-  // TODO: This might not work. check if entity exists first here?
-  // FixME
-  private function add_entities_to_civicrm($changed_entities) {
-    foreach ($changed_entities as $entity => $values) {
-      $result = civicrm_api3($entity, 'create', $values);
-      if ($result['is_error'] == '1') {
-        $this->log("API Error occured: {$result['error_message']}");
+  private function check_nav_before_vs_civi($entities, $contact_id) {
+    try {
+      $this->check_civi_contact_data($entities['Contact'], $contact_id);
+      $this->check_civi_address_data($entities['Address'], $contact_id);
+    } catch (Exception $e) {
+
+    }
+  }
+
+  private function check_civi_address_data($data, $contact_id) {
+    // TODO: implement
+  }
+
+  private function check_civi_contact_data($data, $contact_id) {
+    // nothing to do here
+    if (!isset($data)) {
+      return;
+    }
+    // get Contact
+    foreach ($data as $contact_data) {
+      if ($contact_data['contact_type'] == 'Individual') {
+        $result = civicrm_api3('Contact', 'get', array(
+          'sequential' => 1,
+          'id' => $contact_id,
+        ));
+        if ($result['is_error'] == '1') {
+          throw new Exception("Couldn't find Contact with ID {$contact_id}");
+        }
+        $this->compare_data($contact_data, $result['values']);
       }
     }
+  }
+
+  /**
+   * @param $nav_data
+   * @param $civi_query_result
+   *
+   * @throws \Exception
+   */
+  private function compare_data($nav_data, $civi_query_result) {
+    foreach ($nav_data as $nav_civi_key => $nav_value) {
+      if ($civi_query_result[$nav_civi_key] != $nav_value) {
+        throw new Exception("Value Mismatch - Nav Data: '$civi_query_result[$nav_civi_key]' != '{$nav_value}' (CiviData)");
+      }
+    }
+  }
+
+  // FixMe: Obsolete
+  private function get_civi_entity($entity, $contact_id) {
+    // add additional custom return fields
+    if ($entity == "Contact") {
+      $values['return'] = ["custom_41"];
+      $values['id']     = $contact_id;
+    } else {
+      $values['contact_id'] = $contact_id;
+    }
+    $result = civicrm_api3($entity, 'get', $values);
+    if ($result['is_error'] == 1) {
+      return "";
+    }
+    return $result['values'];
   }
 
   private function add_nav_id_to_contact($contact_id, $nav_id) {
@@ -97,13 +152,18 @@ class CRM_Nav_Handler_ContactHandler extends CRM_NAV_Handler_HandlerBase {
     }
   }
 
-  private function get_update_values() {
-    $changed_civi_entities = array();
-    $changed_civi_entities[] = $this->record->get_changed_contact_values();
-    $changed_civi_entities[] = $this->record->get_changed_address_values();
-    $changed_civi_entities[] = $this->record->get_changed_phone_values();
-    $changed_civi_entities[] = $this->record->get_changed_mail_values();
-    $changed_civi_entities[] = $this->record->get_changed_website_values();
+  /**
+   * @param $type (before|after)
+   *
+   * @return array
+   */
+  private function get_update_values($type) {
+    $changed_civi_entities   = [];
+    $changed_civi_entities[] = $this->record->get_changed_contact_values($type);
+    $changed_civi_entities[] = $this->record->get_changed_address_values($type);
+    $changed_civi_entities[] = $this->record->get_changed_phone_values($type);
+    $changed_civi_entities[] = $this->record->get_changed_mail_values($type);
+    $changed_civi_entities[] = $this->record->get_changed_website_values($type);
 
     return $changed_civi_entities;
   }
