39e4d40dc550d55a98691eb1c5757a1f5aea202b
Philipp Batroff <batroff@systopia.de>
2018-08-29 09:53:56 +0200

    API Skeleton code

--- nav.civix.php
+++ nav.civix.php
@@ -1,19 +1,4 @@
 <?php
-/*-------------------------------------------------------+
-| Navision API Tools                                     |
-| Heinrich Böll Stiftung                                 |
-| Copyright (C) 2018 SYSTOPIA                            |
-| Author: P. Batroff (batroff@systopia.de)               |
-| http://www.systopia.de/                                |
-+--------------------------------------------------------+
-| This program is released as free software under the    |
-| Affero GPL license. You can redistribute it and/or     |
-| modify it under the terms of this license which you    |
-| can read by viewing the included agpl.txt or online    |
-| at www.gnu.org/licenses/agpl.html. Removal of this     |
-| copyright header is strictly prohibited without        |
-| written permission from the original author(s).        |
-+--------------------------------------------------------*/
 
 // AUTO-GENERATED FILE -- Civix may overwrite any changes made to this file
 
