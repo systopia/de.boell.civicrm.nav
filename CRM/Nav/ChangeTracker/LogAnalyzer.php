a98df8cd2d5356879c0f4691fc1f3d196b5b9f03
Philipp Batroff <batroff@systopia.de>
2018-10-31 14:11:21 +0100

    class bare

---
+++ CRM/Nav/ChangeTracker/LogAnalyzer.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| Navision API Tools                                     |
+| Heinrich Böll Stiftung                                 |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: P. Batroff (batroff@systopia.de)               |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+class CRM_Nav_ChangeTracker_LogAnalyzer {
+
+  public function __construct() {
+  }
+
+  public function get_logging_data($entity, $timestamp) {
+
+  }
+}
